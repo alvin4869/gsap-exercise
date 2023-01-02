d4a0841d0c202a15b67232cdc5088f33c55e872c
瑜瑜 <ybkjb14@gmail.com>
2023-01-02 18:42:34 +0800

    動態生成svg

--- js/all.js
+++ js/all.js
@@ -2,9 +2,19 @@
 gsap.to('#js-kv-slide',{
   x:3500,
   duration: 30,
-  repeat:2,
+  repeat:0,
   repeatDelay: 0,
   yoyo:false,
   ease:Power0.easeNone
 })
+const NS = 'http://www.w3.org/2000/svg';
+const main = document.querySelector('main')
+let svg = document.createElementNS(NS,'svg')
+svg.setAttribute('viewBox','0 0 2000 2000')
+svg.setAttribute('width','400');
+svg.setAttribute('height','400');
+svg.setAttribute('id','svg1')
+main.appendChild(svg)
+let svg1 = document.querySelector('#svg1')
+console.log(svg1);
   
