e6b59780de0ae02b630cf938d6cfeba217e0ef3e
瑜瑜 <ybkjb14@gmail.com>
2023-01-15 18:41:22 +0800

    第一組svg set到定位點

--- js/main.js
+++ js/main.js
@@ -21,5 +21,40 @@ gsap.to(".js-kv-slide-item", {
   modifiers: {
     x: gsap.utils.unitize(x => parseFloat(x) % 3500) //force x value to be between 0 and 500 using modulus
   },
-  repeat: -1
+  repeat: 0
 });
+gsap.set('g [filter="url(#__lottie_element_53)"]',{
+  x:500,
+  y:1400,
+  scale:0.2,
+})
+gsap.set('g [filter="url(#__lottie_element_33)"]',{
+  x:1400,
+  y:1400,
+  scale:0.2,
+})
+gsap.set('g [filter="url(#__lottie_element_43)"]',{
+  x:1200,
+  y:1300,
+  scale:0.4,
+})
+gsap.set('g [filter="url(#__lottie_element_23)"]',{
+  x:1300,
+  y:1800,
+  scale:0.65,
+})
+gsap.timeline().to('g [filter="url(#__lottie_element_33)"]',{
+  // rotate:360,
+  transformOrigin:'50% 50%',
+  duration:1,
+  repeat:-1,
+  ease: "none",
+  x:1100,
+  y:1600,
+}).to('g [filter="url(#__lottie_element_33)"]',{
+  x:800,
+  y:1400,
+  duration:1,
+  repeat:-1,
+  ease: "none",
+})
